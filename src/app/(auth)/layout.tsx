7e69f945bdb2fbf7c280579c3f58b803b0fce503
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-04 18:22:53 +0500

    add sign-in and sign-up page in route and one layout.tsx for auth folders

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        {/* nav bar  */}
+        <nav className="flex justify-between items-center">
+          <Image src={"/logo.svg"} alt="logo" width={100} height={100} />
+          <div className="flex items-center gap-4">
+            <Button variant={"secondary"}>Sign Up</Button>
+          </div>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthLayout;
