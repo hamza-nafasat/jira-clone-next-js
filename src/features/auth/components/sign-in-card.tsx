b342c650b696cd499dd126d408f61ec947587de7
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-07 11:43:59 +0500

    make sign in card

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,12 +1,33 @@
 "use client";
 import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().trim().email({ message: "Enter a valid email" }),
+  password: z.string().min(1, { message: "Password is required" }),
+});
 
 export const SignInCard = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center p-7 ">
@@ -17,16 +38,51 @@ export const SignInCard = () => {
       </div>
 
       <CardContent className="p-7 ">
-        <form className="space-y-4">
-          <Input
-            required
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter your email"
-            disabled={false}
-          />
-        </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="email" placeholder="Enter your email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="password" placeholder="Enter your password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button disabled={false} size={"lg"} className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button variant={"secondary"} size={"lg"} className="w-full">
+          <FcGoogle className="mr-2 size-5" />
+          Login with Google
+        </Button>
+        <Button variant={"secondary"} size={"lg"} className="w-full">
+          <FaGithub className="mr-2 size-5" />
+          Login with Github
+        </Button>
       </CardContent>
     </Card>
   );
