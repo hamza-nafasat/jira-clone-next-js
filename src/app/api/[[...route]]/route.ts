9caf634575aba3f95bbafc49fb1e94aa5ac9d240
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-21 19:37:48 +0500

    add validation and some changes

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "hello world",
+  });
+});
+
+app.get("/project/:projectId", (c) => {
+  const { projectId } = c.req.param();
+  return c.json({
+    projectId,
+  });
+});
+
+export const GET = handle(app);
