7723d45d67badbed99cdf731c24702afec1c526b
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-21 19:37:57 +0500

    add validation and some changes

---
+++ src/features/auth/schemas.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const loginSchema = z.object({
+  email: z.string().trim().email({ message: "Enter a valid email" }),
+  password: z.string().min(1, { message: "Password is required" }),
+});
+
+const signUpSchema = z.object({
+  name: z.string().trim().min(1, { message: "Name is required" }),
+  email: z.string().trim().email({ message: "Enter a valid email" }),
+  password: z.string().min(1, { message: "Password is required" }),
+});
+export { loginSchema, signUpSchema };
