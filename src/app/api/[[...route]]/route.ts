5595860fbe590b365929e72b06c7ac1c845c7038
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-25 19:49:43 +0500

    add routes

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,19 +1,11 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import authRoutes from "@/features/auth/server/route";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "hello world",
-  });
-});
-
-app.get("/project/:projectId", (c) => {
-  const { projectId } = c.req.param();
-  return c.json({
-    projectId,
-  });
-});
+export const routes = app.route("/auth", authRoutes);
 
 export const GET = handle(app);
+export const POST = handle(app);
+export type AppType = typeof routes;
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,13 +1,39 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
-import { loginSchema } from "../schemas";
+import { loginSchema, signUpSchema } from "../utils/schemas";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { deleteCookie, setCookie } from "hono/cookie";
+import { AUTH_COOKIE_NAME, COOKIEOPTIONS } from "../utils/constants";
 
-const app = new Hono();
+const app = new Hono()
+  // login api
+  // -------------
+  .post("/login", zValidator("json", loginSchema), async (c) => {
+    const { email, password } = c.req.valid("json");
+    const { account } = await createAdminClient();
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE_NAME, session.secret, COOKIEOPTIONS);
+    return c.json({ success: true, message: "Login Successfully" });
+  })
+  // register api
+  // -------------
+  .post("/register", zValidator("json", signUpSchema), async (c) => {
+    const { email, name, password } = c.req.valid("json");
+    const { account } = await createAdminClient();
 
-app.post("/login", zValidator("json", loginSchema), (c) => {
-  return c.json({
-    message: "success",
+    const user = await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE_NAME, session.secret, COOKIEOPTIONS);
+
+    return c.json({ success: true, message: "Registered Successfully", data: user });
+  })
+  // logout
+  // ------
+  .post("/logout", async (c) => {
+    deleteCookie(c, AUTH_COOKIE_NAME);
+    return c.json({ success: true, message: "Logout Successfully" });
   });
-});
 
 export default app;
