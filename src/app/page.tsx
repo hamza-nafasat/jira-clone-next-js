15520d9b94229c60dc94be824ae4e5a983b37d86
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-27 18:46:02 +0500

    complete login sign up and logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,13 @@
-"use client";
+import UserButton from "@/features/auth/components/user-button";
+import { getCurrentUserAction } from "@/features/auth/server/actions";
+import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-import useGetCurrentUser from "@/features/auth/api/use-getCurrentUser";
-import useLogout from "@/features/auth/api/use-logout";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-export default function Home() {
-  const router = useRouter();
-  const { data, isLoading } = useGetCurrentUser();
-  const { mutate } = useLogout();
-
-  useEffect(() => {
-    if (!isLoading && !data) router.push("/sign-in ");
-    else console.log("data", data);
-  }, [data, isLoading, router]);
-  return isLoading ? (
-    <div>Loading...</div>
-  ) : (
+export default async function Home() {
+  const currentUser = await getCurrentUserAction();
+  if (!currentUser) redirect("/sign-in");
+  return (
     <div className="m-2">
-      only visible to logged in users
-      <Button onClick={() => mutate({})}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
