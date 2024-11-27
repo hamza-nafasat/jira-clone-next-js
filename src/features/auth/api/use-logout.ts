14144e3d68d9f4c70d6e4b3a59e26696a9c3f0a5
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-27 18:46:22 +0500

    complete login sign up and logout

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,11 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { client } from "@/lib/rpc";
 import { InferRequestType, InferResponseType } from "hono";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 type RequestType = InferRequestType<(typeof client.api.auth.logout)["$post"]>;
 
 const useLogout = () => {
+  const router = useRouter();
   const queryClient = useQueryClient();
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async () => {
@@ -14,6 +16,7 @@ const useLogout = () => {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["current"] });
+      router.refresh();
     },
   });
   return mutation;
