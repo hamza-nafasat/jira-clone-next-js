3c7d4785c62f8857610e453c820266f6063c4dea
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-27 18:46:06 +0500

    complete login sign up and logout

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,11 @@
-"use client";
-
 import { SignInCard } from "@/features/auth/components/sign-in-card";
+import { getCurrentUserAction } from "@/features/auth/server/actions";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const currentUser = await getCurrentUserAction();
+  if (currentUser) redirect("/");
   return (
     <div>
       <SignInCard />
