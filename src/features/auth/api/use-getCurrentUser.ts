581a0a2adb166bdf35f23b24670c33d6a8c6cd2e
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-27 18:46:10 +0500

    complete login sign up and logout

--- src/features/auth/api/use-getCurrentUser.ts
+++ src/features/auth/api/use-getCurrentUser.ts
@@ -1,18 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { client } from "@/lib/rpc";
-import { InferRequestType, InferResponseType } from "hono";
-
-type ResponseType = InferResponseType<(typeof client.api.auth.current)["$get"]> | null;
-type RequestType = InferRequestType<(typeof client.api.auth.current)["$get"]>;
 
 const useGetCurrentUser = () => {
-  const query = useQuery<ResponseType, Error, RequestType>({
+  const query = useQuery({
     queryKey: ["current"],
     queryFn: async () => {
       const response = await client.api.auth.current["$get"]();
       if (!response.ok) return null;
       const { data } = await response.json();
-      return { data };
+      return data;
     },
   });
   return query;
