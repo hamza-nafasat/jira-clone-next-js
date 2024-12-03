6f0e6c15d59f9f32217ed708dff89dcb852dc092
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-12-03 18:47:06 +0500

    add workspace form and make auth complete

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,8 +1,13 @@
 import { getCurrentUserAction } from "@/features/auth/server/actions";
+import CreateWorkspaceForm from "@/features/workspaces/components/create-workspace-form";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const currentUser = await getCurrentUserAction();
   if (!currentUser) redirect("/sign-in");
-  return <div className="m-2">this is a home page </div>;
+  return (
+    <div className="m-2 bg-neutral-600 p-4">
+      <CreateWorkspaceForm />
+    </div>
+  );
 }
