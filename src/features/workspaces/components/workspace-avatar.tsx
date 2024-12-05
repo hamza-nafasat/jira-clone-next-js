f7acd432d3e3f8e8398080565ac0daaee5bcf2a3
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-12-05 19:33:26 +0500

    add workspace and members

---
+++ src/features/workspaces/components/workspace-avatar.tsx
@@ -0,0 +1,28 @@
+import { Avatar, AvatarFallback } from "@/app/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+interface WorkspaceAvatarProps {
+  image?: string;
+  name: string;
+  className?: string;
+}
+
+const WorkspaceAvatar = ({ image, name, className }: WorkspaceAvatarProps) => {
+  if (image) {
+    return (
+      <div className={cn("size-10 relative rounded-md overflow-hidden")}>
+        <Image src={image} alt={name} width={48} height={48} className={"object-cover"} />
+      </div>
+    );
+  }
+  return (
+    <Avatar className={cn("rounded-md size-10 ", className)}>
+      <AvatarFallback className="text-white bg-blue-600 rounded-md font-semibold text-lg uppercase">
+        {name?.charAt(0)?.toUpperCase()}
+      </AvatarFallback>
+    </Avatar>
+  );
+};
+
+export default WorkspaceAvatar;
