862161a29250002c8d70e3e0fee36486f716ebca
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-12-12 12:36:01 +0500

    FIX WORKSPACES

--- src/app/components/workspace-switcher.tsx
+++ src/app/components/workspace-switcher.tsx
@@ -4,9 +4,18 @@ import useGetAllWorkspaces from "@/features/workspaces/apis/use-get-all-workspac
 import WorkspaceAvatar from "@/features/workspaces/components/workspace-avatar";
 import { RiAddCircleFill } from "react-icons/ri";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 const WorkspaceSwitcher = () => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
   const { data: workspaces } = useGetAllWorkspaces();
+
+  const onSelect = (id: string) => {
+    if (id == "#") return;
+    router.push(`/workspaces/${id}`);
+  };
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
@@ -14,20 +23,29 @@ const WorkspaceSwitcher = () => {
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
 
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId || ""}>
         <SelectTrigger className="w-full bg-neutral-300 font-medium p-1">
           <SelectValue placeholder="No workspaces selected" />
         </SelectTrigger>
 
         <SelectContent>
-          {workspaces?.documents?.map((workspace) => (
-            <SelectItem key={workspace.$id} value={workspace.$id}>
-              <div className="flex justify-start items-center gap-3 font-medium">
-                <WorkspaceAvatar image={workspace.image} name={workspace.name} />
-                <span className="truncate">{workspace.name}</span>
+          {workspaces?.total ? (
+            workspaces?.documents?.map((workspace: any) => (
+              <SelectItem key={workspace.$id} value={workspace.$id}>
+                <div className="flex cursor-pointer justify-start items-center gap-3 font-medium">
+                  <WorkspaceAvatar image={workspace.image} name={workspace.name} />
+                  <span className="truncate">{workspace.name}</span>
+                </div>
+              </SelectItem>
+            ))
+          ) : (
+            <SelectItem key={1} value={"#"}>
+              <div className=" pointer-events-none cursor-not-allowed flex justify-start items-center gap-3 font-medium">
+                <WorkspaceAvatar name={"No Workspaces"} />
+                <span className="truncate">No Workspace</span>
               </div>
             </SelectItem>
-          ))}
+          )}
         </SelectContent>
       </Select>
     </div>
