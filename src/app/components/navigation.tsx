84bcfb69f3b8989202273c9a362bbd8345a6357f
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-12-12 12:35:53 +0500

    FIX NAVIGATION

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -5,6 +5,7 @@ import { SettingsIcon, UserIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { GoCheckCircle, GoCheckCircleFill, GoHome, GoHomeFill } from "react-icons/go";
+
 const routes = [
   {
     label: "Home",
@@ -34,10 +35,9 @@ const routes = [
 
 const Navigation = () => {
   const pathname = usePathname();
-  console.log(pathname);
   return (
     <ul className="flex flex-col ">
-      {routes.map((item) => {
+      {routes?.map((item) => {
         const isActive = pathname === item.href;
         const Icon = isActive ? item.activeIcon : item.icon;
         return (
