ab3b642b31090d02018982d3d1029036058aaa06
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-12-20 15:07:42 +0500

    add nuqs

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -2,13 +2,36 @@
 
 import ResponsiveModel from "@/components/responsive-model";
 import CreateWorkspaceForm from "./create-workspace-form";
+import useCreateWorkspaceModal from "../hooks/user-create-workspace-modal";
 
 const CreateWorkspaceModal = () => {
+  const { isOpen, setIsOpen } = useCreateWorkspaceModal();
   return (
-    <ResponsiveModel open onOpenChange={() => {}}>
+    <ResponsiveModel open={isOpen} onOpenChange={setIsOpen}>
       <CreateWorkspaceForm />
     </ResponsiveModel>
   );
 };
 
 export default CreateWorkspaceModal;
+
+
+[
+  ["Parsons Infrastructure Organization (PTG)", 144000, 52000, 18000, 214000],
+  ["Other Subsidiary 1", 120000, 30000, 15000, 165000],
+  ["Other Subsidiary 2", 130000, 40000, 12000, 182000],
+  ["Other Subsidiary 3", 110000, 25000, 11000, 146000]
+]
+
+[{
+  Parsons Infrastructure Organization (PTG): 144000,
+},
+{
+  Other Subsidiary 1: 120000,
+},
+{
+  Other Subsidiary 2: 130000,
+},
+{
+  Other Subsidiary 3: 110000,
+}]
